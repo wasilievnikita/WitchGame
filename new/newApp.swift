bda27b87029bdbf7bd367e1fa50d553923eba797
wasilievnikita <wasilievnikita@gmail.com>
2023-11-06 20:11:53 +0300

    Initial Commit

---
+++ new/newApp.swift
@@ -0,0 +1,17 @@
+//
+//  newApp.swift
+//  new
+//
+//  Created by Никита Васильев on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
